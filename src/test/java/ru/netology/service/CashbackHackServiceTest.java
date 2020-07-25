6418ac38c5e0e236ca135cb383ff9b5becf46558
Ivan Grebenschikov <albonigrum@gmail.com>
2020-07-25 12:34:26 +0300

    Add testng tests for CashbackHackService. Update build.gradle

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
+
+    @Test
+    public void shouldTestRemainIfAmount0() {
+        assertEquals(cashbackHackService.remain(0), 1000);
+    }
+    @Test
+    public void shouldTestRemainIfAmount400() {
+        assertEquals(cashbackHackService.remain(400), 600);
+    }
+    @Test
+    public void shouldTestRemainIfAmount1000() {
+        assertEquals(cashbackHackService.remain(1000), 1000);
+    }
+}
