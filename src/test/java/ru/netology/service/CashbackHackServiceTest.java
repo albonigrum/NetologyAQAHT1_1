4ed0499a2eb71eab64cfb1f9e1e5aca70d4dbe1d
Ivan Grebenschikov <albonigrum@gmail.com>
2020-07-25 12:59:41 +0300

    Add Junit-jupiter-engine and tests with Junit-jupiter-api

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 import org.junit.Test;
 
+
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
@@ -18,5 +19,18 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldTestRemainIfAmount1000() {
         assertEquals(0, cashbackHackService.remain(1000));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldTestRemainIfAmount0Jupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(1000, cashbackHackService.remain(0));
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldTestRemainIfAmount400Jupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(600, cashbackHackService.remain(400));
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldTestRemainIfAmount1000Jupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(0, cashbackHackService.remain(1000));
     }
 }
